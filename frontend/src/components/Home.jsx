f417e0b0c3ff209795cacdd7e4a6e8693dce70f1
Keaton Singer <keatonsinger@Keatons-MacBook-Pro-2.local>
2022-10-19 17:15:54 -0700

    Sliding Panel Fixed

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -20,16 +20,15 @@ function Home() {
   }
 */
 
+  const user = JSON.parse(localStorage.getItem('user'));
+  const [name, setName] = React.useState(user ? user.name : '');
+  const [icon, setPic] = React.useState(user ? user.pic : '');
+  const [error, setError] = React.useState('Logged Out');
   const [toggle, setToggle] = React.useState({
     panelTransform: 'translateX(100%)',
     eventsContainerDim: 'rgb(214, 196, 171)',
     eventDim: '#2E5584',
   });
-
-  const user = JSON.parse(localStorage.getItem('user'));
-  const [name, setName] = React.useState(user ? user.name : '');
-  const [icon, setPic] = React.useState(user ? user.pic : '');
-  const [error, setError] = React.useState('Logged Out');
 
   const history = useNavigate();
 
@@ -43,9 +42,13 @@ function Home() {
   
   const togglePanel = (action) => {
     if (action === 'close') {
-      toggle
+      setToggle({
+        panelTransform: 'translateX(100%)',
+        eventsContainerDim: 'rgb(214, 196, 171)',
+        eventDim: '#2E5584',
+      });
     } else {
-      () => setToggle({
+      setToggle({
         panelTransform: 'translateX(0%)',
         eventsContainerDim: 'rgb(87, 79, 69)',
         eventDim: '#15263b',
@@ -58,25 +61,25 @@ function Home() {
  
     return (
       <div>
-        <div id='sidePanel' style={{transform: "translateX(0%)"}}>
+        <div id='sidePanel' style={{transform: toggle.panelTransform}}>
           <div id='profileContainer'>
             <div id='profileName'>{name ? name : ''}</div>
             <div id='profilePicture'><img src = {icon ? icon : ''}/></div>
           </div>
           <div id='viewProfile'>View Profile</div>
           <div id='createPost'>Create Post</div>
-          <div id='signOut' onClick = {logout}>Logout</div>
+          <div id='signOut' onClick={logout}>Logout</div>
         </div>
         <div>
           <div id='pageHeader'>
             <img src={logo} alt='logo' className='logo'/>
-            <div id='navBarContainer' onClick={togglePanel('open')}>
+            <div id='navBarContainer' onClick={() => togglePanel('open')}>
               <div className='navBarLine'></div>
               <div className='navBarLine'></div>
               <div className='navBarLine'></div>
             </div>
           </div>
-          <div id='eventsContainer' onClick={togglePanel('close')}
+          <div id='eventsContainer' onClick={() => togglePanel('close')}
             style={{backgroundColor: toggle.eventsContainerDim}}>
             <div className='event' style={
               {backgroundColor: toggle.eventDim}
