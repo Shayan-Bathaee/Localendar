58aa5fe03458c34fd12f7193859fcdc865aca24e
sbathaee <sbathaee@ucsc.edu>
2022-10-24 21:58:19 -0700

    added event table and hardcoded event values

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -1,5 +1,9 @@
 
--- Populate Your Tables Here --
+-- Populate user table with hardcoded values --
 INSERT INTO users(email, pw, username) VALUES ('bob@ucsc.com', 'password', 'Bob');
 INSERT INTO users(email, pw, username) VALUES ('ann@ucsc.com', 'password', 'Ann');
 INSERT INTO users(email, pw, username) VALUES ('steve@ucsc.com', 'password', 'Steve');
+
+-- Populate event table with hardcoded values --
+INSERT INTO events(eventname, eventemail, eventdate, eventtime, eventlocation, eventdescription) VALUES ("Bob's pie eating contest", "bob@ucsc.com", "2022-12-2", "95060", "eat pies");
+INSERT INTO events(eventname, eventemail, eventdate, eventtime, eventlocation, eventdescription) VALUES ("Ann's fundraiser", "ann@ucsc.com", "2023-2-01", "92115", "raising funds");
--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,4 +1,15 @@
 
--- Your database schema goes here --
+-- user profile table --
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(email VARCHAR(255), pw VARCHAR(255), username VARCHAR(255));
+
+-- event table -- 
+DROP TABLE IF EXISTS events;
+CREATE TABLE events(
+    eventname VARCHAR(255),
+    eventemail VARCHAR(255), -- email of the poster
+    eventdate DATE, -- YYYY-MM-DD
+    eventtime TIME, -- hh:mm:ss
+    eventlocation VARCHAR(255), -- zip code for now, but may change later
+    eventdescription VARCHAR(2000) -- allow for a long description
+);
