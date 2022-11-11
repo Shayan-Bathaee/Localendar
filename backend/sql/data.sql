926c7d55a6b948829360f8046e493a2ed0adac03
ljfong <ljfong0503@gmail.com>
2022-11-11 00:21:32 -0800

    Changed coords to floats

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -4,5 +4,5 @@ INSERT INTO users(email, pic, username) VALUES ('bob@ucsc.com', 'https://lh3.goo
 INSERT INTO users(email, pic, username) VALUES ('steve@ucsc.com', 'https://lh3.googleusercontent.com/a/ALm5wu2LYxyLQVPRYjO2SJEuz-wYzOulc7Vil4iWVu85=s96-c', 'Steve');
 
 -- Populate event table with hardcoded values --
-INSERT INTO events(eventname, email, eventdate, eventtime, eventlocation, eventcoords, eventdescription) VALUES ('Bobs pie eating contest', 'bob@ucsc.com', '2022-12-02', '04:20:00', '95060', '37.052748, -122.111126', 'eat pies');
-INSERT INTO events(eventname, email, eventdate, eventtime, eventlocation, eventcoords, eventdescription) VALUES ('Steves fundraiser', 'steve@ucsc.com', '2023-02-01', '06:30:00', '92115', '32.7576, -117.070688', 'raising funds');
+INSERT INTO events(eventname, email, eventdate, eventtime, eventlocation, eventcoords, eventdescription) VALUES ('Bobs pie eating contest', 'bob@ucsc.com', '2022-12-02', '04:20:00', '95060', '{37.052748, -122.111126}', 'eat pies');
+INSERT INTO events(eventname, email, eventdate, eventtime, eventlocation, eventcoords, eventdescription) VALUES ('Steves fundraiser', 'steve@ucsc.com', '2023-02-01', '06:30:00', '92115', '{32.7576, -117.070688}', 'raising funds');
