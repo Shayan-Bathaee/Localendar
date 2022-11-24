9a8fb4b570201b560288f234230bd30e73eb083a
suttekar <shrutiutekar7@gmail.com>
2022-11-24 06:40:59 -0800

    change url

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -113,7 +113,7 @@ function Home () {
   Geocode.setApiKey('AIzaSyAZwTrchd6eBtPRB7m1VOz5Fh5smHba5Us')
 
   const getEventsFromDB = () => {
-    fetch('https://localendar.herokuapp.com/v0/eventform', {
+    fetch('http://localhost:3010/v0/eventform', {
       method: 'GET'
     })
       .then((res) => {
