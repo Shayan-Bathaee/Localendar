00ba4a1d61014ec74759aca54ff728da7f72c54f
suttekar <shrutiutekar7@gmail.com>
2022-11-24 07:08:30 -0800

    change url

--- frontend/src/components/EventForm.jsx
+++ frontend/src/components/EventForm.jsx
@@ -17,7 +17,7 @@ import PlacesAutocomplete, {
 // eventdescription
 function writeEventToDB (newEvent) {
   /* write event to database */
-  fetch('/v0/eventform', {
+  fetch('https://localendar/v0/eventform', {
     method: 'POST',
     body: JSON.stringify(newEvent),
     headers: {
