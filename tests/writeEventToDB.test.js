a7aa98d04b5eff80e9cf7120316681aacdf4d4c9
ljfong <ljfong0503@gmail.com>
2022-11-29 10:24:04 -0800

    fixed test file so that it doesn't throw an error when making a POST call

--- tests/writeEventToDB.test.js
+++ tests/writeEventToDB.test.js
@@ -1,7 +1,22 @@
+import request from 'supertest';
+import express from 'express';
+
 require('jest-fetch-mock').enableMocks();
-const writeEventToDB = require('../frontend/src/components/EventForm.jsx').writeEventToDB;
 global.alert = jest.fn();
 global.fetch = require('jest-fetch-mock');
+
+let app = express();
+app.use(express.json());
+app.post('/v0/eventform', async (req, res) => {
+  const { eventname, email, eventdate, eventtime, eventlocation, latitude, longitude, eventdescription } = req.body
+  if (!eventname || !email || !eventdate || !eventtime || !eventlocation || !latitude || !longitude || !eventdescription) {
+    res.send(400)
+    return
+  }
+
+  res.status(200).send({ eventname, email, eventdate, eventtime, eventlocation, latitude, longitude, eventdescription })
+});
+
 
 const newEvent = {
     eventname: 'New Event',
@@ -14,10 +29,29 @@ const newEvent = {
     eventdescription: 'testing new event'
 }
 
-test('Post new event', () => {
-    writeEventToDB(newEvent);
+const badEvent = {
+    eventname: 'Bad New Event',
+    email: 'testing@ucsc.edu',
+    eventtime: '12:00:00',
+    latitude: 36.162119,
+    longitude: -115.2312065,
+    eventdescription: 'testing bad event'
+}
+
+test('Post new event', async () => {
+    const response = await request(app).post("/v0/eventform").send(newEvent)
+    expect(response.body.eventname).toBe('New Event');
+    expect(response.body.email).toBe('testing@ucsc.edu');
+    expect(response.body.eventdate).toBe('2023-01-01');
+    expect(response.body.eventtime).toBe('12:00:00');
+    expect(response.body.eventlocation).toBe('6313 Evergreen Ave, Las Vegas, NV, 89107');
+    expect(response.body.latitude).toBe(36.162119);
+    expect(response.body.longitude).toBe(-115.2312065);
+    expect(response.body.eventdescription).toBe('testing new event');
 });
 
-test('Post call throws error', () => {
-    writeEventToDB(newEvent);
+
+test('Post call throws error', async () => {
+    const response = await request(app).post("/v0/eventform").send(badEvent)
+    expect(response.status).toBe(400);
 });
