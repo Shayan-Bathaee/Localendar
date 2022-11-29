ef31bfade7dc2416e973d4b9eb2254bb859c5d54
alhochen <alhochen@ucsc.edu>
2022-11-29 03:06:10 -0800

    deleting unused file

--- frontend/src/__tests__/login.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-
-import Login from '../components/Login.jsx';
-import App from '../App.jsx';
-
-test('login page', () => {
-  render(<App />);
-});
